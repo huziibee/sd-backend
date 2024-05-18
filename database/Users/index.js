69b6ae5e2af50c76ade299c243c7c5a26346d616
WitsPram <2551233@students.wits.ac.za>
2024-05-18 16:44:57 +0200

    tests pass

--- database/Users/index.js
+++ database/Users/index.js
@@ -1,49 +1,51 @@
-// index.js
 const { ConnectionPool } = require('mssql');
 const config = require('../config.js');
 
 async function readerUserData(userID) {
+    const pool = new ConnectionPool(config.connectionString);
     try {
-        const pool = new ConnectionPool(config.connectionString);
         await pool.connect();
         const result = await pool.request().query(`select profile_pic_url, user_type from [User] where email = '${userID}'`);
-        await pool.close();
         return result.recordset[0];
     } catch (err) {
         console.error(err.message);
         throw err;
+    } finally {
+        await pool.close();
     }
 }
 
 async function insertUserData(email, profilePicUrl) {
+    const pool = new ConnectionPool(config.connectionString);
     try {
-        const pool = new ConnectionPool(config.connectionString);
         await pool.connect();
-        const result = await pool.request().query(`
+        await pool.request().query(`
             INSERT INTO [User] (email, profile_pic_url, user_type, created_at)
             VALUES ('${email}', '${profilePicUrl}', 'User', GETDATE())
         `);
-        await pool.close();
         return { message: 'Success' };
     } catch (err) {
         console.error(err.message);
         throw err;
+    } finally {
+        await pool.close();
     }
 }
 
 async function updateUserData(email, profilePicUrl) {
+    const pool = new ConnectionPool(config.connectionString);
     try {
-        const pool = new ConnectionPool(config.connectionString);
         await pool.connect();
-        const result = await pool.request().query(`
+        await pool.request().query(`
             UPDATE [User] SET profile_pic_url = '${profilePicUrl}'
             WHERE email = '${email}'
         `);
-        await pool.close();
         return { message: 'Success' };
     } catch (err) {
         console.error(err.message);
         throw err;
+    } finally {
+        await pool.close();
     }
 }
 
