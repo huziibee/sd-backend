57825978fa3d7d02cf4786458114d9931b7ca541
dullseagull <2543772@students.wits.ac.za>
2024-05-18 17:23:07 +0200

    databaseDoneTest

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,15 @@
 module.exports = {
     collectCoverage: true,
     coverageReporters: ['text', 'cobertura'],
-}
+
+    // jest.config.json
+
+
+    "transform": {
+      "^.+\\.jsx?$": "babel-jest"
+    },
+    "moduleNameMapper": {
+      "\\.(css|less|scss|sass)$": "identity-obj-proxy"
+    }
+  }
+  
