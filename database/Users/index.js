819a42f59efe8795a539443d18fc73f5e6639a99
dullseagull <2543772@students.wits.ac.za>
2024-05-18 16:23:04 +0200

    100%3/6pass

--- database/Users/index.js
+++ database/Users/index.js
@@ -1,131 +1,50 @@
-const {sql , ConnectionPool } = require('mssql');
+// index.js
+const { ConnectionPool } = require('mssql');
 const config = require('../config.js');
-
-const { connectionString } = require('../config.js');
 
 async function readerUserData(userID) {
     try {
-        // Create a new connection pool
-        const pool = new ConnectionPool(connectionString);
+        const pool = new ConnectionPool(config.connectionString);
         await pool.connect();
-
-        console.log("Reading rows from the Table...");
-        const resultSet = await pool.request().query(`select profile_pic_url, user_type from [User] where email = '${userID}'`);
-
-        let user = null;
-        resultSet.recordset.forEach(row => {
-            user = row;
-        });
-
-        // Close the connection pool
+        const result = await pool.request().query(`select profile_pic_url, user_type from [User] where email = '${userID}'`);
         await pool.close();
-
-        return user;
+        return result.recordset[0];
     } catch (err) {
         console.error(err.message);
-        throw err; // Re-throw the error to handle it in the caller
+        throw err;
     }
 }
-// async function readerUserData(userID) {
-//     try {
-//         var poolConnection = await sql.connect(connectionString);
 
-//         console.log("Reading rows from the Table...");
-//         var resultSet = await poolConnection.request().query(`select profile_pic_url , user_type  from [User] where email = '${userID}'`);
-
-//         let user = null; 
-//         resultSet.recordset.forEach(row => {
-//             user = row;
-//         });
-
-//         // close connection only when we're certain application is finished
-//         await poolConnection.close();
-
-//         return user;
-//     } catch (err) {
-//         console.error(err.message);
-//         throw err; // Re-throw the error to handle it in the caller
-//     }
-// }
-
-async function insertUserData(email, profile_pic_url) {
+async function insertUserData(email, profilePicUrl) {
     try {
-        // Connect to the database
-
-        const poolConnection = await sql.connect(config);
-
-        // console.log("smurf", userExists)
-
-        // Insert the row into the table
-
-        
-
-        console.log("Inserting!!")
-        const resultSet = await poolConnection.request().query(`IF NOT EXISTS (SELECT 1 FROM [User] WHERE email = '${email}')
-        BEGIN
+        const pool = new ConnectionPool(config.connectionString);
+        await pool.connect();
+        const result = await pool.request().query(`
             INSERT INTO [User] (email, profile_pic_url, user_type, created_at)
-            VALUES ('${email}', '${profile_pic_url}', 'Applicant', GETDATE());
-        END
+            VALUES ('${email}', '${profilePicUrl}', 'User', GETDATE())
         `);
-
-        // Close the connection
-        await poolConnection.close();
-
-        let returnObj = null;
-
-        if (resultSet.rowsAffected[0] == 1) {
-            returnObj = { "message" : "Success"};
-        }else{
-            returnObj = { "message" : "Failure"};
-        }
-        console.log(returnObj)
-        return returnObj;        
-
-        
+        await pool.close();
+        return { message: 'Success' };
     } catch (err) {
         console.error(err.message);
-        throw err; // Re-throw the error to handle it in the caller
+        throw err;
     }
 }
 
-async function updateUserData(email, profile_pic_url) {
+async function updateUserData(email, profilePicUrl) {
     try {
-        // Connect to the database
-
-        const poolConnection = await sql.connect(config);
-
-
-        console.log("Updating!!")
-        const resultSet = await poolConnection.request().query(`
-        UPDATE [User]
-        SET profile_pic_url = '${profile_pic_url}'
-        WHERE email = '${email}';`);
-
-        await poolConnection.close();
-
-        let returnObj = null;
-
-        if (resultSet.rowsAffected[0] == 1) {
-            returnObj = { "message" : "Success"};
-        }else{
-            returnObj = { "message" : "Failure"};
-        }
-        console.log(returnObj)
-        return returnObj; 
-        
+        const pool = new ConnectionPool(config.connectionString);
+        await pool.connect();
+        const result = await pool.request().query(`
+            UPDATE [User] SET profile_pic_url = '${profilePicUrl}'
+            WHERE email = '${email}'
+        `);
+        await pool.close();
+        return { message: 'Success' };
     } catch (err) {
         console.error(err.message);
-        throw err; // Re-throw the error to handle it in the caller
+        throw err;
     }
 }
 
-
-// insertUserData("fhddbsjkf", "d")
-// updateUserData("fhddbdsdsjkf", "f")
-
-module.exports = {
-    readerUserData,
-    insertUserData,
-    updateUserData
-};
-// module.exports = insertUserData;
+module.exports = { readerUserData, insertUserData, updateUserData };
