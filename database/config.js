2bd27e7ef6d788b74f5f2e14d1905412b1a1e713
huziibee <mhbangie@gmail.com>
2024-05-26 00:55:50 +0200

    test

--- database/config.js
+++ database/config.js
@@ -1,3 +1,5 @@
 const connectionString = `Server=tcp:ezezimalidbs.database.windows.net,1433;Initial Catalog=ezezimalidb;Persist Security Info=False;User ID=ezezimali_admin;Password=Ezimal11;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;`;
 
+// test
+
 module.exports = { connectionString };
