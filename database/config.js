731fee432f4bb5e5fd2121d2652c36704a66d103
WitsPram <2551233@students.wits.ac.za>
2024-05-27 00:38:47 +0200

    revert secret

--- database/config.js
+++ database/config.js
@@ -1,3 +1,8 @@
-const connectionString = process.env.CONNECTION_STRING 
+// const connectionString = `Server=tcp:ezezimalidbs.database.windows.net,1433;Initial Catalog=ezezimalidb;Persist Security Info=False;User ID=ezezimali_admin;Password=Ezimal11;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;`;
+
+// const connectionString = `Server=tcp:ezezimali.database.windows.net,1433;Initial Catalog=ezezimali;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;Authentication="Active Directory Default";`;
+
+
+const connectionString = `Server=tcp:ezezimali.database.windows.net,1433;Initial Catalog=ezezimali;Persist Security Info=False;User ID=ezezimali_admin;Password=Ezimal11;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;`;
 
 module.exports = { connectionString };
