3f382b7973c3f550f7293a5f0ac4ec95e3497077
WitsPram <2551233@students.wits.ac.za>
2024-05-27 00:23:03 +0200

    Secret added

--- database/config.js
+++ database/config.js
@@ -1,8 +1,3 @@
-// const connectionString = `Server=tcp:ezezimalidbs.database.windows.net,1433;Initial Catalog=ezezimalidb;Persist Security Info=False;User ID=ezezimali_admin;Password=Ezimal11;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;`;
-
-// const connectionString = `Server=tcp:ezezimali.database.windows.net,1433;Initial Catalog=ezezimali;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;Authentication="Active Directory Default";`;
-
-
-const connectionString =  secrets.CONNECTION_STRING
+const connectionString = process.env.CONNECTION_STRING 
 
 module.exports = { connectionString };
